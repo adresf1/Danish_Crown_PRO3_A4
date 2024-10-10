40ef1140ea36a1e78cfd7dd1ac8819deb137f4ae
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-10-10 16:14:03 +0200

    saka

---
+++ src/main/java/via/pro3/Main.java
@@ -0,0 +1,9 @@
+package via.pro3;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/main/java/via/pro3/service/Danish_Crown_Impl.java
@@ -0,0 +1,10 @@
+package via.pro3.service;
+
+// Importer de genererede klasser fra gRPC
+import io.grpc.stub.StreamObserver;
+import via.pro3.Danish_Crown_PRO3.grpc.Danish_Crown_ImplGrpc;
+import via.pro3.Danish_Crown_PRO3.grpc.AnimalRequest;
+
+
+public class Danish_Crown_Impl extends Danish_Crown_ImplGrpc.Danish_Crown_ImplImplBase {
+}
